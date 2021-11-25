fa164684a11cfd59c75a108d2419371d68aaca7f
ElJohnnie <johnfollmer1995@gmail.com>
2021-11-25 13:49:45 -0300

    feat: first commit and sigle

---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="score">
+    Jogador <span>{{ win_count }}</span> x
+    <span>{{ lose_count }}</span> Computador
+  </section>
+</template>
+<script>
+export default {
+  props: ['win_count', 'lose_count'],
+};
+</script>
+
+<style scoped>
+section.score {
+  border-bottom: 1px solid black;
+  padding: 24px;
+  font-size: 18px;
+}
+span {
+  padding: 8px;
+  font-weight: bold;
+  border: 1px solid black;
+}
+</style>
